b372eedfd305ea50563ecafa6d9db3984fced076
morganherlocker <morgan.herlocker@gmail.com>
2014-12-10 17:52:16 -0500

    fixed bench casing

--- bench.js
+++ bench.js
@@ -1,5 +1,5 @@
 var linestring = require('./');
-var Benchmark = require('Benchmark');
+var Benchmark = require('benchmark');
 var fs = require('fs');
 
 var suite = new Benchmark.Suite('turf-linestring');
