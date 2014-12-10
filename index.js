ad35abeb10c4173cf445961f6021db1b87d81c97
morganherlocker <morgan.herlocker@gmail.com>
2014-12-09 19:27:15 -0500

    update styling

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 module.exports = function(coordinates, properties){
   if(!coordinates) return new Error('No coordinates passed')
-  var linestring = { 
+  return { 
     "type": "Feature",
     "geometry": {
       "type": "LineString",
       "coordinates": coordinates
     },
     "properties": properties
-  }
-  return linestring
+  };
 }
--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 var test = require('tape');
-var lineString = require('./')
+var lineString = require('./');
 
 test('lineString', function(t){
-  t.plan(4)
+  t.plan(4);
 
-  var line = lineString([[5, 10], [20, 40]], {name: 'test line'})
+  var line = lineString([[5, 10], [20, 40]], {name: 'test line'});
 
   t.ok(line);
-  t.equal(line.geometry.coordinates[0][0], 5)
-  t.equal(line.geometry.coordinates[1][0], 20)
-  t.equal(line.properties.name, 'test line')
+  t.equal(line.geometry.coordinates[0][0], 5);
+  t.equal(line.geometry.coordinates[1][0], 20);
+  t.equal(line.properties.name, 'test line');
 });
