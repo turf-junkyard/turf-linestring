6b3425830ecd1f7eb36c4d7f428a01f9c7fe8e46
Lyzi Diamond <lyzidiamond@gmail.com>
2015-01-13 21:57:09 -0800

    Change example to Reykjavik, Iceland

--- index.js
+++ index.js
@@ -7,11 +7,22 @@
  * @param {Object} properties an Object consisting of key-value pairs to add as properties
  * @return {LineString} a LineString feature
  * @example
- * var linestring1 = turf.linestring([[102.0, -10.0], [103.0, 1.0], [104.0, 0.0], [130.0, 4.0]])
- * var linestring2 = turf.linestring([[102.0, -10.0], [103.0, 1.0], [104.0, 0.0], [130.0, 4.0]],
- *   {name: 'line 1', distance: 145})
- * console.log(linestring1)
- * console.log(linestring2)
+ * var linestring1 = turf.linestring([
+ *	[-21.964416, 64.148203],
+ *	[-21.956176, 64.141316],
+ *	[-21.93901, 64.135924],
+ *	[-21.927337, 64.136673]
+ * ]);
+ * var linestring2 = turf.linestring([
+ *	[-21.929054, 64.127985],
+ *	[-21.912918, 64.134726],
+ *	[-21.916007, 64.141016],
+ * 	[-21.930084, 64.14446]
+ * ], {name: 'line 1', distance: 145});
+ *
+ * //=linestring1
+ *
+ * //=linestring2
  */
 module.exports = function(coordinates, properties){
   if(!coordinates) return new Error('No coordinates passed')
