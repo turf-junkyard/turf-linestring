9f3249d9f299340159ff3f0e16a681f603a1b518
Tom MacWright <tom@macwright.org>
2015-02-05 16:48:21 -0500

    Add category

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
  * coordinate array. Properties can be added optionally.
  *
  * @module turf/linestring
+ * @category helper
  * @param {Array<Array<Number>>} coordinates an array of Positions
  * @param {Object} properties an Object of key-value pairs to add as properties
  * @return {LineString} a LineString feature
